19687a1ee30fb901b43af93e2d7372f87924618a
flower HC <723524841@qq.com>
2025-08-29 12:42:24 +0800

    FFramework游戏开发框架 0.1.21版本。
    更新UIRoot方法，实现一键添加对应组件，取消拖拽Prefab添加UIRoot。

--- FFramework/Utility/UIKit/UIRoot.cs
+++ FFramework/Utility/UIKit/UIRoot.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace FFramework.Kit
 {
@@ -7,8 +8,6 @@ namespace FFramework.Kit
     /// </summary>
     public class UIRoot : SingletonMono<UIRoot>
     {
-        #region UI层级  
-
         public Transform BackgroundLayer;       //背景层 - 静态背景
         public Transform PostProcessingLayer;   //后期处理层 - 后期处理效果
         public Transform ContentLayer;          //内容层 - 主要UI功能
@@ -16,9 +15,77 @@ namespace FFramework.Kit
         public Transform GuideLayer;            //引导层 - 引导玩家操作    
         public Transform DebugLayer;            //调试层 - 创建和调试UI   
 
-        #endregion
+        [Button("创建UI层级")]
+        private void CrateUILayer()
+        {
+            this.name = "UIRoot";
+            // 必要组件
+            if (!TryGetComponent<Canvas>(out _)) gameObject.AddComponent<Canvas>();
+            if (!TryGetComponent<CanvasScaler>(out _)) gameObject.AddComponent<CanvasScaler>();
+            if (!TryGetComponent<GraphicRaycaster>(out _)) gameObject.AddComponent<GraphicRaycaster>();
+
+            // EventSystem
+            if (transform.Find("EventSystem") == null)
+            {
+                var es = new GameObject("EventSystem");
+                es.transform.SetParent(transform, false);
+                es.AddComponent<UnityEngine.EventSystems.EventSystem>();
+                es.AddComponent<UnityEngine.EventSystems.StandaloneInputModule>();
+            }
+            // 添加UI层
+            BackgroundLayer = CreateAndAddUIlayerInGameObject("BackgroundLayer", this.transform, false, false, false);
+            PostProcessingLayer = CreateAndAddUIlayerInGameObject("PostProcessingLayer", this.transform, true, true, false);
+            ContentLayer = CreateAndAddUIlayerInGameObject("ContentLayer", this.transform, true, true, false);
+            PopupLayer = CreateAndAddUIlayerInGameObject("PopupLayer", this.transform, false, true, false);
+            GuideLayer = CreateAndAddUIlayerInGameObject("GuideLayer", this.transform, true, true, false);
+            DebugLayer = CreateAndAddUIlayerInGameObject("DebugLayer", this.transform, true, true, false);
+        }
+
+        /// <summary>
+        /// 创建并添加UI层
+        /// </summary>
+        /// <param name="uiLayerName">ui层名称</param>
+        /// <param name="parent">父级变换</param>
+        /// <param name="interactable">是否可交互</param>
+        /// <param name="blocksRaycasts">是否阻挡射线</param>
+        /// <param name="ignoreParentGroups">是否忽略父级组</param>
+        private Transform CreateAndAddUIlayerInGameObject(string uiLayerName, Transform parent, bool interactable, bool blocksRaycasts, bool ignoreParentGroups)
+        {
+            // 优先查找已存在的层级
+            Transform exist = parent.Find(uiLayerName);
+            if (exist != null)
+            {
+                // 查找或添加CanvasGroup，并设置属性
+                var canvasGroup = exist.GetComponent<CanvasGroup>();
+                if (canvasGroup == null) canvasGroup = exist.gameObject.AddComponent<CanvasGroup>();
+                canvasGroup.interactable = interactable;
+                canvasGroup.blocksRaycasts = blocksRaycasts;
+                canvasGroup.ignoreParentGroups = ignoreParentGroups;
+
+                return exist;
+            }
+
+            // 创建新层级
+            GameObject uiLayer = new GameObject(uiLayerName);
+            uiLayer.transform.SetParent(parent, false); // 保持局部坐标
+            uiLayer.transform.localPosition = Vector3.zero;
+            uiLayer.transform.localRotation = Quaternion.identity;
+            uiLayer.transform.localScale = Vector3.one;
+
+            // 添加CanvasGroup并设置属性
+            var newCanvasGroup = uiLayer.GetComponent<CanvasGroup>();
+            if (newCanvasGroup == null) newCanvasGroup = uiLayer.AddComponent<CanvasGroup>();
+            newCanvasGroup.interactable = interactable;
+            newCanvasGroup.blocksRaycasts = blocksRaycasts;
+            newCanvasGroup.ignoreParentGroups = ignoreParentGroups;
+
+            return uiLayer.transform;
+        }
     }
 
+    /// <summary>
+    /// UI层级枚举
+    /// </summary>
     public enum UILayer
     {
         BackgroundLayer,
